2be75f1a6a101f13e165f00f28440676da2edbb0
Joan <jollmat@hotmail.com>
2022-04-06 22:01:00 +0200

    Alerts

--- src/app/services/alerts.service.ts
+++ src/app/services/alerts.service.ts
@@ -3,7 +3,6 @@ import { Subject } from 'rxjs';
 import { STORE_KEYS_CONSTANTS } from '../model/constants/store-keys.constants';
 import { AlertInterface, UserAlerts } from '../model/interfaces/alert.interface';
 import { ShopProductInterface } from '../model/interfaces/shop-product.interface';
-import { LoginService } from './login.service';
 import { ProductService } from './product.service';
 import { ShopProductService } from './shop-product.service';
 
@@ -24,7 +23,7 @@ export class AlertsService {
 
   loadUnreadAlerts(): UserAlerts {
     //console.log('AlertsService.loadUnreadAlerts()');
-    let userAlerts = {
+    let userAlerts: UserAlerts = {
       updateDate: new Date().getTime(),
       alerts: []
     }
@@ -32,15 +31,44 @@ export class AlertsService {
     const storedUserAlerts = localStorage.getItem(STORE_KEYS_CONSTANTS.PS_USER_ALERTS);
     if (storedUserAlerts) {
       const storedUserAlertsObj = (JSON.parse(storedUserAlerts)) as UserAlerts;
+      // TODO Remove user alerts from unexisting products or shops
       return storedUserAlertsObj;
     }
     return userAlerts;
   }
 
-  saveUnreadAlerts() {
-    //console.log('AlertsService.saveUnreadAlerts()');
+  updateAccessDateAlerts() {
     this._userAlerts.updateDate = new Date().getTime();
+    this.saveAlerts();
+    
+  }
+
+  saveAlerts() {
+    // TODO Remove user alerts from unexisting products or shops
     localStorage.setItem(STORE_KEYS_CONSTANTS.PS_USER_ALERTS, JSON.stringify(this._userAlerts));
+  }
+
+  toggleReadAlert(alert: AlertInterface) {
+    alert.read = !alert.read;
+    const storedUserAlerts = localStorage.getItem(STORE_KEYS_CONSTANTS.PS_USER_ALERTS);
+    if (storedUserAlerts) {
+      const storedUserAlertsObj = (JSON.parse(storedUserAlerts)) as UserAlerts;
+      const storedAlert = storedUserAlertsObj.alerts.find((_alert) => {
+        return _alert.date === alert.date &&
+               _alert.productBarcode === alert.productBarcode &&
+               _alert.shopId === alert.shopId &&
+               _alert.currentPrice === alert.currentPrice &&
+               _alert.lastPrice === alert.lastPrice
+      });
+      storedAlert.read = !storedAlert.read;
+      localStorage.setItem(STORE_KEYS_CONSTANTS.PS_USER_ALERTS, JSON.stringify(storedUserAlertsObj));
+    }
+  }
+
+  getUnreadAlerts(): number {
+    return this._userAlerts.alerts.filter((_alert) => { 
+      return !_alert.read;
+    }).length;
   }
 
   calculateAlerts(doSave: boolean) {
@@ -83,14 +111,20 @@ export class AlertsService {
             date: shopProductHistory[shopProductHistory.length -1].updateDate,
             lastPrice: shopProductHistory[shopProductHistory.length -2].price,
             productBarcode: shopProductHistory[shopProductHistory.length -1].productBarcode,
-            shopId: shopProductHistory[shopProductHistory.length -1].shopId
+            shopId: shopProductHistory[shopProductHistory.length -1].shopId,
+            read: false
           });
         }        
       }
     });
+    console.log(this._userAlerts);
 
     this.userAlerts.next(this._userAlerts);
     
-    if (doSave) { this.saveUnreadAlerts(); }
+    if (doSave) { 
+      this.updateAccessDateAlerts(); 
+    } else {
+      this.saveAlerts();
+    }
   }
 }
