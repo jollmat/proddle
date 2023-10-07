f79613e08890faa2dc5d059c686f2abe977d0c43
Joan <jollmat@hotmail.com>
2023-10-07 11:14:25 +0200

    Enable cloud mode

--- src/config/app-config.constant.ts
+++ src/config/app-config.constant.ts
@@ -2,6 +2,6 @@ import { AppConfigInterface } from '../app/model/interfaces/app-config.interface
 
 export const APP_CONFIG: AppConfigInterface = {
   defaultLanguage: 'ca',
-  cloudMode: false,
+  cloudMode: true,
   bannedIPs: []
 };
